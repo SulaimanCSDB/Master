9f7c148b10f0ef9b745eaeeac4c4cf27c2529498
Sulaiman <sulaimancsdb@gmail.com>
2023-12-18 08:08:37 +0430

    added Calcalator.js

---
+++ 10-Calculator.js
@@ -0,0 +1,17 @@
+//10
+class Calculator{
+
+    Addition(num1 , num2){
+        return num1 + num2;
+
+    }
+
+    Subtraction(num1 , num2){
+        return num1 - num2;
+    }
+};
+
+    const m1 = new Calculator();
+
+console.log(m1.Addition(5,7));
+console.log(m1.Subtraction(10,5));
