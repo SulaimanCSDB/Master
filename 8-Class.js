b8ee2e4c9589c8db7d7674f0da4071ccf090210a
Sulaiman <sulaimancsdb@gmail.com>
2023-12-18 08:07:45 +0430

    added Class.js

---
+++ 8-Class.js
@@ -0,0 +1,14 @@
+//8 
+class Book{
+
+    constructor(title, author){
+
+    this.title = title;
+    this.author = author;
+
+    }
+
+};
+    let m1 = new Book("Alsunne", "IbnHambal");
+
+    console.log(m1.author);
