e4f74fdbd7fa16453f934cfce20d07eded6ad3ae
Sulaiman <sulaimancsdb@gmail.com>
2023-12-18 08:08:57 +0430

    added baseClass.js

---
+++ 11-baseClass.js
@@ -0,0 +1,17 @@
+//11
+class Shape{
+
+    constructor(color){
+
+        this.color = color;
+
+    }
+
+    calculatArea(){
+    return "Area Calculation not implemented";
+    }
+};
+
+let m2 = new Shape("red");
+
+console.log(m2.calculatArea());
